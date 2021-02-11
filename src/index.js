2b7d987a6b851c2062e72b0913f1f5f6ae8edc25
leon kranich <leon_kranich@yahoo.de>
2021-02-11 16:16:47 +0100

    ebay api won't work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello from src/index.js!");
+const axios = require('axios')
+
+axios
+	.get('https://www.reddit.com/r/programming.json')
+	.then((response) => {
+		console.log(response)
+	})
+	.catch((error) => {
+		console.error(error)
+	});

9061221694cb72174b8034e093c9c5b1ff933c43
leon kranich <leon_kranich@yahoo.de>
2021-02-11 18:26:36 +0100

    some tests, but not happy so far

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const axios = require('axios')
+const fetch = require('isomorphic-fetch');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-axios
-	.get('https://www.reddit.com/r/programming.json')
-	.then((response) => {
-		console.log(response)
-	})
-	.catch((error) => {
-		console.error(error)
-	});
+// fetch("https://www.etsy.com/search?q=jeans", {
+// 	mode: 'no-cors'
+// })
+//   .then(response => response.text())
+// 	.then(text => new JSDOM(text))
+//   .then((dom) => {
+//     console.log(dom.window.document.querySelector("h2").textContent);
+//   });
+
+(async () => {
+  const response = await fetch('https://example.com');
+  const text = await response.text();
+  const dom = await new JSDOM(text);
+  console.log(dom.window.document.querySelector("h1").textContent);
+})()
